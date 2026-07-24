a12f4d098229df787484f4218290c975a2d34338
Muragharajendra <muragharajendra@gmail.com>
2026-07-25 02:06:15 +0530

    gh

--- doc_parser/chapter_chunking.py
+++ doc_parser/chapter_chunking.py
@@ -4,5 +4,6 @@ from preprocessing_extracted_text import text_preprocess, open_file
 text, inp_txt= open_file()
 
 all_chapters=text_preprocess(text, inp_txt)  # chapter wise chunked list
-# print(all_chapters[0])
+
+print(all_chapters[1])
 
