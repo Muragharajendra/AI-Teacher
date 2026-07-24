921423e722ec1b2ce6b41c4682ada418a161410b
Muragharajendra <muragharajendra@gmail.com>
2026-07-24 23:53:38 +0530

    hello

--- doc_parser/preprocessing_extracted_text.py
+++ doc_parser/preprocessing_extracted_text.py
@@ -84,8 +84,8 @@ def text_preprocess(text):
             all_chapters.append(chapter)
         ch_index+=1
         
-    print(all_chapters[1])
-    # print(chapters[5])
+    # print(all_chapters[0])
+    print(chapters[5])
 
 
 
