4e9b74ac3e874ee7a04fc42f528e2c719cbdff51
Muragharajendra <muragharajendra@gmail.com>
2026-07-26 23:55:04 +0530

    add

--- doc_parser/chunking/chapter_chunking.py
+++ doc_parser/chunking/chapter_chunking.py
@@ -5,5 +5,7 @@ text, inp_txt= open_file()
 
 all_chapters=text_preprocess(text, inp_txt)  # chapter wise chunked list
 
-print(all_chapters[2])
+# print(all_chapters[2])
 
+print(type(all_chapters[0]))
+print(all_chapters[0][:3000])
