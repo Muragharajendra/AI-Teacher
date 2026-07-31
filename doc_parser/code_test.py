8c327f0b4caf099068060f649f99158e97959040
Muragharajendra <muragharajendra@gmail.com>
2026-07-31 22:27:26 +0530

    dd

--- doc_parser/code_test.py
+++ doc_parser/code_test.py
@@ -7,5 +7,5 @@ for i in all_chapters:
             all_chap.append(i)
 print(all_chap[1])  
 print("Hello!")
-print("Lowda focus on execution. One day everything will be fine.")
+print("focus on execution. One day everything will be fine.")
 print("Believe yourself")
