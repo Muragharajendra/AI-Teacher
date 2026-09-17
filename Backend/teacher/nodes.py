fec1fff574da8dec3f839cc948da677c49ce7843
Muragharajendra S P <muragharajendra@gmail.com>
2026-09-17 10:44:26 +0530

    upgrade1

--- Backend/teacher/nodes.py
+++ Backend/teacher/nodes.py
@@ -50,11 +50,12 @@ def router_node(state):
         }
 
 
-def retrieval_node(state):
+def retrieval_node(query):
+    
     query = (
-        state.get("Optimised_query")
-        or state.get("user_query", "")
-    ).strip()
+            state.get("Optimised_query")
+            or state.get("user_query", "")
+        ).strip()
     print("QUERY for chunks retrieval:::", query)
     retrieval_method = (
         state.get("retrieval_method")
